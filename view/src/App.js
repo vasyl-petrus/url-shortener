b7716d2a88cd15e11d52bb718217a97370b23b27
Vasyl Petrus <petrusvasyl.j@gmail.com>
2021-08-02 16:02:05 +0300

    create simple form

--- view/src/App.js
+++ view/src/App.js
@@ -1,7 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  const [inputValue, setInputValue] = useState('');
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState();
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:5000/shorten', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ originalUrl: inputValue }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
+    console.log({ isLoading, data });
+  };
+
+  return (
+    <div className='App'>
+      <form className='App' onSubmit={onSubmit}>
+        <input onChange={handleChange} />
+        <button>Generate short url</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
