861ca01957b556485a08748037dea78d5cb99c0f
Vasyl Petrus <petrusvasyl.j@gmail.com>
2021-08-02 16:05:24 +0300

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
 const connection = require('./config/db');
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 connection.once('open', () => console.log('DB Connected'));
 connection.on('error', () => console.log('Error'));
 
+app.use(cors())
 app.use(
   express.json({
     extended: false,
